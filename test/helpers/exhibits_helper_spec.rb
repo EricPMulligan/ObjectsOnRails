4a651ed34404d767f6f74a27e11d97568a8ec681
Eric Mulligan <eric.mulligan@rcmp-grc.gc.ca>
2015-02-19 17:59:37 -0500

    Still not working

--- test/helpers/exhibits_helper_spec.rb
+++ test/helpers/exhibits_helper_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../spec_helper_lite'
 require_relative '../../app/helpers/exhibits_helper'
 
+class PicturePostExhibit; def initialize(*args); end; end
+class TextPostExhibit; def initialize(*args); end; end
+
 describe ExhibitsHelper do
   before do
     @it = Object.new
@@ -10,14 +13,14 @@ describe ExhibitsHelper do
 
   it 'decorates picture posts with a PicturePostExhibit' do
     post = mock('Post')
-    post.stub(:picture?).returns(true)
-    @it.exhibit(post, @context).must_be_kind_of(mock('PicturePostExhibit'))
+    post.stubs(:picture?).returns(true)
+    @it.exhibit(post, @context).must_be_kind_of(PicturePostExhibit)
   end
 
   it 'decorates text posts with a TextPostExhibit' do
     post = mock('Post')
-    post.stub(:picture?).returns(false)
-    @it.exhibit(post, @context).must_be_kind_of(mock('TextPostExhibit'))
+    post.stubs(:picture?).returns(false)
+    @it.exhibit(post, @context).must_be_kind_of(TextPostExhibit)
   end
 
   it "leaves objects it doesn't know about alone" do
